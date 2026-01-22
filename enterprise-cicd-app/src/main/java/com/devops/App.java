1afb7527683e8cde5a27c487c557eb379384c41f
saikumar <saikumarnallagatla@gmail.com>
2026-01-22 08:13:04 +0530

    github actions

---
+++ enterprise-cicd-app/src/main/java/com/devops/App.java
@@ -0,0 +1,10 @@
+package com.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps World!");
+    }
+    public String getGreeting() {
+        return "Hello DevOps!";
+    }
+}
